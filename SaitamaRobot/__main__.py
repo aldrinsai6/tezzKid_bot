1724b7f1730fcc31559fb6d62c8b31e8ebdf04d5
Badboy <77886836+aldrinsai6@users.noreply.github.com>
2021-03-15 15:07:48 +0530

    Update_main_.py
    tada

--- SaitamaRobot/__main__.py
+++ SaitamaRobot/__main__.py
@@ -205,15 +205,15 @@ def start(update: Update, context: CallbackContext):
                                 context.bot.username)),
                          InlineKeyboardButton(
                              text=" 🔔 Updates ",
-                             url="https://t.me/TG_BotZ")
+                             url="https://t.me/Botzilla")
                      ],
                      [
                         InlineKeyboardButton(
                             text=" 🔮Help ",
-                            url="https://t.me/ELIZABETHTG_bot?start=help"),
+                            url="https://t.me/Xtra_Mega_bot?start=help"),
                          InlineKeyboardButton(
                             text=" 🚴Made by 🛸 ",
-                             url="https://t.me/rosebakthan")        
+                             url="https://t.me/Father_of_all_Evils")        
                        
                      ],
                      [
